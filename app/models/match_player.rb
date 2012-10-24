936dec6d97920c860a754b560baebce5a8dfeda7
James Logsdon <dwarf@girsbrain.org>
2012-10-24 15:23:42 -0400

    Remove an unnecessary relation

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -1,7 +1,6 @@
 class MatchPlayer < ActiveRecord::Base
   belongs_to :match,  counter_cache: :player_count
   belongs_to :player, counter_cache: :match_count
-  has_many   :match_players, class_name: 'MatchPlayer', foreign_key: 'match_id', primary_key: 'match_id'
 
   validates :winner, inclusion: {in: [true, false]}
   validates :team, numericality: {greater_than: 0}
